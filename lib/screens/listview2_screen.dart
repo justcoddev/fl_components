1bc1273fbeec963d84780e03ea16829b896cc0e2
justcoddev <edisonpaulcrz@gmail.com>
2022-06-13 15:25:08 -0500

    Taps sobre ListTile

--- lib/screens/listview2_screen.dart
+++ lib/screens/listview2_screen.dart
@@ -10,12 +10,21 @@ class Listview2Screen extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(
           title: const Text('Listview Tipo 2'),
+          elevation: 0,
+          backgroundColor: Colors.indigo,
         ),
         body: ListView.separated(
           itemCount: options.length,
           itemBuilder: (context, index) => ListTile(
             title: Text(options[index]),
-            trailing: const Icon(Icons.arrow_forward_ios_outlined),
+            trailing: const Icon(
+              Icons.arrow_forward_ios_outlined,
+              color: Colors.indigo,
+            ),
+            onTap: () {
+              final game = options[index];
+              print(game);
+            },
           ),
           separatorBuilder: (_, __) => const Divider(),
         ));
