11adab52c50cf7c77166d406e7602dc45e1fb7b5
justcoddev <edisonpaulcrz@gmail.com>
2022-06-14 16:32:36 -0500

    Diseño HomeScreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-   
   const HomeScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('HomeScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Components Flutter'),
+        elevation: 0,
       ),
+      body: ListView.separated(
+          itemBuilder: (context, index) => ListTile(
+                leading: const Icon(Icons.access_time_outlined),
+                title: const Text('Nombre de la ruta'),
+                onTap: () {},
+              ),
+          separatorBuilder: (_, __) => const Divider(),
+          itemCount: 100),
     );
   }
 }
